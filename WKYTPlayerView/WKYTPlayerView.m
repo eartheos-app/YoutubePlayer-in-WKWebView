8fd12695910ec8f01901cf8f0e143e366afa34d2
Rob Delafuente <robdelafuente@me.com>
2018-01-09 22:20:47 -0800

    [IPHONEX] Add back scalesPageToFit, with iPhoneX specific code

--- WKYTPlayerView/WKYTPlayerView.m
+++ WKYTPlayerView/WKYTPlayerView.m
@@ -1019,7 +1019,7 @@ NSString static *const kWKYTPlayerSyndicationRegexPattern = @"^https://tpc.googl
     
     // WKWebView equivalent for UIWebView's scalesPageToFit
     // http://stackoverflow.com/questions/26295277/wkwebview-equivalent-for-uiwebviews-scalespagetofit
-    NSString *jScript = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width, viewport-fit=cover'); document.getElementsByTagName('head')[0].appendChild(meta);”;
+    NSString *jScript = @"var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'width=device-width, viewport-fit=cover'); document.getElementsByTagName('head')[0].appendChild(meta);";
     
     WKUserScript *wkUScript = [[WKUserScript alloc] initWithSource:jScript injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES];
     WKUserContentController *wkUController = [[WKUserContentController alloc] init];
